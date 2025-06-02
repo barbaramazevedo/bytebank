840eca9d38981dd4dec31b2b2919aaddac7e8afa
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-06-02 11:26:33 -0300

    first commit

---
+++ src/types/Conta.ts
@@ -0,0 +1,20 @@
+import { Transacao } from "./Transacao";
+
+export class Conta {
+    nome: string
+    saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
+    transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: any) => {
+        if (key === "data") {
+            return new Date(value)
+        }
+        return value
+    }) || [];
+
+    constructor(nome: string) {
+        this.nome = nome;
+    }
+}
+
+const conta = new Conta("Jaoana da Silva Oliveira");
+
+export default conta;
